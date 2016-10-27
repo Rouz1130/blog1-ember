11b99d7e54546fda3736423f49559e43c54af085
rouz majlessi <rmajlessi@gmail.com>
2016-10-27 13:10:42 -0700

    add model for questions and populate its properties /att

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,20 +4,17 @@ var questions = [{
   id: 1,
   author: "Bob Costas",
   content: "Sports",
-  notes: "NBA Finals",
-  image: "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg"
+  notes: "NBA Finals"
 }, {
   id: 2,
   author: "Tommy Hilfiger",
   content: "Fashion",
-  notes: "Jackets",
-  image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg"
+  notes: "Jackets"
 }, {
   id: 3,
   author: "Anderson Cooper",
   content: "News",
-  notes: "Election",
-  image: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg"
+  notes: "Election"
 }];
 
 export default Ember.Route.extend({
