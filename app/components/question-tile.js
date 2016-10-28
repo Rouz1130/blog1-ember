e838e4e150f50252659ca157816d97a70d460edb
rouz majlessi <rmajlessi@gmail.com>
2016-10-28 13:25:46 -0700

    add ability to show answer in question js file

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isAnswerShowing: false,
+  actions: {
+    answerShow: function() {
+      this.set('isAnswerShowing', true);
+    }
+  }
 });
